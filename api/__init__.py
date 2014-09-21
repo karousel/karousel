4d7d4cca6fd1f56caa9a141393b942740fb2d648
Mihir Singh <git.service@mihirsingh.com>
2014-09-21 07:56:22 -0400

    Established an S3 connection and created a bucket (if it's missing)

--- api/__init__.py
+++ api/__init__.py
@@ -13,3 +13,11 @@ from user import UserModel
 from photo import PhotoModel
 
 database.create_tables([PhotoModel, AlbumModel, UserModel, CollectionModel], True)
+
+from boto.s3.connection import S3Connection
+s3 = S3Connection(config.get('S3', 'AccessKey'), config.get('S3', 'SecretKey'))
+
+if s3.lookup(config.get('S3', 'Bucket')) is None:
+
+    s3.create_bucket(config.get('S3', 'Bucket'))
+
