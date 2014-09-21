0fe827aca992955021c44982985bf805dfd20884
Mihir Singh <git.service@mihirsingh.com>
2014-09-21 09:45:33 -0400

    Implemented POST for collections resources

--- api/collection.py
+++ api/collection.py
@@ -1,8 +1,32 @@
+from flask import abort, request
 from . import CollectionModel, AuthenticatedResource
 
 class CollectionsResource (AuthenticatedResource):
 
     def get (self):
+
+        collections = [{
+                        'id':collection.id,
+                        'name':collection.name,
+                       } for collection in CollectionModel.select()]
+
+        return collections
+
+    def post (self):
+
+        name = request.form.get('name')
+
+        if not name:
+
+            abort(400)
+
+        if CollectionModel.select().where(CollectionModel.name == name).count() == 1:
+
+            abort(409)
+
+        CollectionModel.create(
+            name = name
+        )
 
         collections = [{
                         'id':collection.id,
