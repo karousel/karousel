d9d8e1eb2dd5c6b04f3038f975469940a415ec1f
Mihir Singh <git.service@mihirsingh.com>
2014-11-11 23:27:09 -0500

    Created a configuration type which configures the database session and HTTP router

--- karousel.go
+++ karousel.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/citruspi/Karousel-API/handlers"
@@ -8,17 +10,42 @@ import (
 
 	r "github.com/dancannon/gorethink"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
 )
 
+type Configuration struct {
+	Web struct {
+		Port string
+	}
+	Database struct {
+		Host string
+		Port string
+		Name string
+	}
+}
+
 var (
+	config  Configuration
 	session *r.Session
 	err     error
 )
 
 func main() {
+	data, err := ioutil.ReadFile("karousel.conf")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(data, &config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	session, err = r.Connect(r.ConnectOpts{
-		Address:  "localhost:28015",
-		Database: "karousel",
+		Address:  fmt.Sprintf("%v:%v", config.Database.Host, config.Database.Port),
+		Database: config.Database.Name,
 	})
 
 	if err != nil {
@@ -32,5 +59,5 @@ func main() {
 
 	router.GET("/users/", handlers.GetUserResource)
 
-	router.Run(":8000")
+	router.Run(fmt.Sprintf(":%v", config.Web.Port))
 }
